6f5b772d98de583c3fb0c0a4cfb3c78635d8da4a
Tiara <wtiarar@gmail.com>
2026-01-31 18:03:20 +0700

    menambahkan variabel input

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # program sederhana jika kelipatan 3 tik, kalau mialkan kelipatan 5 boom , dan keliatapatan 3 dan 5 tik 
 # boom program hanya sampai 50 munculkan semua angkanya
 
+max = input()
 for x in range(1,150):
     if x%3==0 : print("tik")
     elif x%5==0: print("boom")
