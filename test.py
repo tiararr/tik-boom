bff89af95ca60f7595801fe5088c5cd35e3953ba
Tiara <wtiarar@gmail.com>
2026-01-31 17:58:04 +0700

    tambahkan jadi 150

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # program sederhana jika kelipatan 3 tik, kalau mialkan kelipatan 5 boom , dan keliatapatan 3 dan 5 tik 
 # boom program hanya sampai 50 munculkan semua angkanya
 
-for x in range(1,50):
+for x in range(1,150):
     if x%3==0 : print("tik")
     elif x%5==0: print("boom")
     elif x%3==0 and x%5==0: print("tik boom")
